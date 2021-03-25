c7158cda84c8a136203f9277c9f4883ee49602d4
StJonny <st.onlyemail@gmail.com>
2021-03-25 18:09:46 +0300

    Separate GLSL and Python code

--- lab1/square.py
+++ lab1/square.py
@@ -14,30 +14,6 @@ vbo_colors = np.array([
 ], dtype='f4')
 indexes = np.array([0, 2, 1, 0, 2, 3], dtype='u4')
 
-vertex_shader = """
-#version 330
-
-in vec3 in_coord;
-in vec3 in_color;
-out vec4 color;
-
-void main() {
-    gl_Position = vec4(in_coord, 1.0);
-    color = vec4(in_color, 1.0);
-}
-"""
-
-fragment_shader = """
-#version 330
-
-in  vec4 color;
-out vec4 f_color;
-
-void main() {
-    f_color = color;
-}
-"""
-
 global_width = 800
 global_height = 800
 
@@ -46,12 +22,13 @@ class Window(mglw.WindowConfig):
     gl_version = (3, 3)
     window_size = (global_width, global_height)
     title = "Colored square"
+    resource_dir = "resources"
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
         # do initialization here
-        self.prog = self.ctx.program(vertex_shader=vertex_shader, fragment_shader=fragment_shader)
+        self.prog = self.load_program("programs/square.glsl")
         self.vao = self.ctx.vertex_array(
             self.prog,
             [
